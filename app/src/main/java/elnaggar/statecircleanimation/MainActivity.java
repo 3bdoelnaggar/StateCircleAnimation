96d9e54221fb21d5a1a907978efbbecafa0682d3
Elnaggar <3bdoelnaggar@gmail.com>
2018-08-29 16:41:38 +0200

    Initial commit

---
+++ app/src/main/java/elnaggar/statecircleanimation/MainActivity.java
@@ -0,0 +1,62 @@
+package elnaggar.statecircleanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean firstTime=true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final StateCircleAnimation stateCircleAnimation = findViewById(R.id.stateCircleView);
+        stateCircleAnimation.setViews(getStateViews(), (TextView) findViewById(R.id.tv_stateName));
+        final EditText stateIDEditText=findViewById(R.id.et_stateId);
+        stateIDEditText.setText("1");
+        Button startAnimationButton=findViewById(R.id.button_startAnimation);
+        startAnimationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int stateId = Integer.parseInt(stateIDEditText.getText().toString());
+                if(stateId>5||stateId<1) {
+                    stateId=1;
+                }
+                stateCircleAnimation.setStateId(stateId);
+                if (firstTime) {
+                    stateCircleAnimation.loadNormalAnimation();
+                    firstTime = false;
+                } else {
+                    stateCircleAnimation.loadReverseAnimation();
+                }
+
+            }
+        });
+
+    }
+
+    private ArrayList<ImageView> getStateViews() {
+        ArrayList<ImageView> imageViews = new ArrayList<>();
+        ImageView imageView1 = findViewById(R.id.iv1);
+        ImageView imageView2 = findViewById(R.id.iv2);
+        ImageView imageView3 = findViewById(R.id.iv3);
+        ImageView imageView4 = findViewById(R.id.iv4);
+        ImageView imageView5 = findViewById(R.id.iv5);
+        imageViews.add(imageView1);
+        imageViews.add(imageView2);
+        imageViews.add(imageView3);
+        imageViews.add(imageView4);
+        imageViews.add(imageView5);
+        return imageViews;
+
+    }
+}
---
+++ app/src/main/java/elnaggar/statecircleanimation/StateCircleAnimation.java
@@ -0,0 +1,371 @@
+package elnaggar.statecircleanimation;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by mahmoud on 14/11/17.
+ */
+
+public class StateCircleAnimation extends View {
+
+    Paint arcPaint1;
+    Paint arcPaint2;
+    Paint arcPaint3;
+    Paint arcPaint4;
+    Paint arcPaint5;
+    private float sweapAngel = 0;
+    private ArrayList<ImageView> imageViews;
+    private TextView tvStateName;
+    private Paint activePaint;
+    private Paint unActivePaint;
+    private int stateId;
+
+    public StateCircleAnimation(Context context) {
+        super(context);
+        setInitialColors();
+    }
+
+
+    public StateCircleAnimation(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setInitialColors();
+
+    }
+
+    void setInitialColors() {
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(getContext().getResources().getColor(R.color.colorAccent));
+
+        paint.setStrokeWidth(getResources().getDimension(R.dimen.circleStorke));
+
+        activePaint = new Paint(paint);
+        activePaint.setColor(getResources().getColor(android.R.color.holo_blue_dark));
+
+        unActivePaint = new Paint(paint);
+        unActivePaint.setColor(getResources().getColor(android.R.color.darker_gray));
+        unActivePaint.setPathEffect(new DashPathEffect(new float[]{10, 5}, 0));
+
+        arcPaint1 = new Paint(unActivePaint);
+        arcPaint2 = new Paint(unActivePaint);
+        arcPaint3 = new Paint(unActivePaint);
+        arcPaint4 = new Paint(unActivePaint);
+        arcPaint5 = new Paint(unActivePaint);
+    }
+
+    public void setViews(ArrayList<ImageView> imageViews, TextView tvStateName) {
+        this.imageViews = imageViews;
+        this.tvStateName = tvStateName;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int parts = (int) (sweapAngel / 72);
+        float remains = sweapAngel % 72;
+        if (remains != 0) {
+            parts += 1;
+        }
+
+        for (int i = 0; i < parts; i++) {
+            if (i == parts - 1) {
+                if (remains == 0) {
+                    drawPart(i + 1, 72, canvas);
+
+                } else {
+                    drawPart(i + 1, remains, canvas);
+
+                }
+            } else {
+                drawPart(i + 1, 72, canvas);
+
+            }
+        }
+    }
+
+    private void drawPart(int part, float sweapAngel, Canvas canvas) {
+        final RectF oval = new RectF(0, 0, getWidth(), getWidth());
+        oval.inset(2, 2);
+        Path redPath = new Path();
+        redPath.arcTo(oval, getStartAngelByPart(part), sweapAngel, true);
+        //canvas.drawPath(redPath, getArcPaint(getStartAngelByPart(part)));
+        canvas.drawPath(redPath, getArcPaintByPart(part));
+
+    }
+
+    int getStartAngelByPart(int part) {
+        int returnedValue = 198 + (part * 72);
+        if (returnedValue > 360) {
+            returnedValue = returnedValue % 360;
+        }
+        return returnedValue;
+
+    }
+
+
+
+
+    Paint getArcPaintByPart(int acrNum) {
+        switch (acrNum) {
+            case 1:
+                return arcPaint1;
+            case 2:
+                return arcPaint2;
+
+            case 3:
+                return arcPaint3;
+
+            case 4:
+                return arcPaint4;
+
+
+            case 5:
+                return arcPaint5;
+
+        }
+        return arcPaint1;
+    }
+
+
+
+
+    public void changeState(int stateID) {
+        switch (stateID) {
+            case 1:
+
+
+                arcPaint1 = new Paint(unActivePaint);
+                arcPaint2 = new Paint(unActivePaint);
+                arcPaint3 = new Paint(unActivePaint);
+                arcPaint4 = new Paint(unActivePaint);
+                arcPaint5 = new Paint(unActivePaint);
+
+
+
+                break;
+            case 2:
+                arcPaint1 = new Paint(activePaint);
+                arcPaint2 = new Paint(unActivePaint);
+                arcPaint3 = new Paint(unActivePaint);
+                arcPaint4 = new Paint(unActivePaint);
+                arcPaint5 = new Paint(unActivePaint);
+
+
+                break;
+
+            case 3:
+
+                arcPaint1 = new Paint(activePaint);
+                arcPaint2 = new Paint(activePaint);
+                arcPaint3 = new Paint(unActivePaint);
+                arcPaint4 = new Paint(unActivePaint);
+                arcPaint5 = new Paint(unActivePaint);
+
+
+
+                break;
+            case 4:
+
+                arcPaint1 = new Paint(activePaint);
+                arcPaint2 = new Paint(activePaint);
+                arcPaint3 = new Paint(activePaint);
+                arcPaint4 = new Paint(unActivePaint);
+                arcPaint5 = new Paint(unActivePaint);
+
+
+
+                break;
+            case 5:
+
+                arcPaint1 = new Paint(activePaint);
+                arcPaint2 = new Paint(activePaint);
+                arcPaint3 = new Paint(activePaint);
+                arcPaint4 = new Paint(activePaint);
+                arcPaint5 = new Paint(activePaint);
+
+                break;
+            case 0:
+            case 6:
+
+                arcPaint1 = new Paint(unActivePaint);
+                arcPaint2 = new Paint(unActivePaint);
+                arcPaint3 = new Paint(unActivePaint);
+                arcPaint4 = new Paint(unActivePaint);
+                arcPaint5 = new Paint(unActivePaint);
+
+                break;
+        }
+
+    }
+
+    private void prepareAnimation() {
+        for (ImageView imageView : imageViews) {
+            imageView.setScaleX(0f);
+            imageView.setScaleY(0f);
+        }
+    }
+
+
+    void loadAnimationOrdaly(final int i) {
+        imageViews.get(i).animate().scaleX(1f).scaleY(1f).setDuration(200).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                int newI = i + 1;
+                if (newI < imageViews.size() && i >= 0) {
+                    loadAnimationOrdaly(newI);
+                }
+                if (i == 3) {
+                    loadCircleAnimation();
+                }
+                if (i == 4) {
+                }
+            }
+        }).start();
+    }
+
+
+    void loadAnimationOrdalyReverse() {
+        for (int i = 0; i < imageViews.size(); i++) {
+            ImageView imageView = imageViews.get(i);
+            final int finalI = i;
+            imageView.animate().scaleX(0f).scaleY(0f).setDuration(200).setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    if (finalI == 4) {
+                        loadNormalAnimation();
+                    }
+
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+        }
+    }
+
+
+    public void loadNormalAnimation() {
+        changeState(stateId);
+        prepareAnimation();
+        loadAnimationOrdaly(0);
+    }
+
+    public void loadReverseAnimation() {
+        loadCircleAnimationReverse();
+    }
+
+    void loadCircleAnimation() {
+        this.setSweapAngel(0);
+        CircleAngleAnimation circleAngelAnimation = new CircleAngleAnimation(CircleAngleAnimation.ZORDER_NORMAL, this, 360);
+        circleAngelAnimation.setDuration(1000);
+        this.startAnimation(circleAngelAnimation);
+
+    }
+
+    void loadCircleAnimationReverse() {
+        this.setSweapAngel(360);
+        CircleAngleAnimation circleAngelAnimation = new CircleAngleAnimation(CircleAngleAnimation.REVERSE, this, 0);
+        circleAngelAnimation.setDuration(1000);
+        circleAngelAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                loadAnimationOrdalyReverse();
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        this.startAnimation(circleAngelAnimation);
+
+    }
+
+
+    public void setSweapAngel(float sweapAngel) {
+
+        this.sweapAngel = sweapAngel;
+    }
+
+    public float getSweapAngel() {
+        return sweapAngel;
+    }
+
+    public void setStateId(int stateId) {
+        this.stateId = stateId;
+    }
+
+    public class CircleAngleAnimation extends Animation {
+
+        private final int type;
+        private StateCircleAnimation circle;
+
+        private float oldAngle;
+        private float newAngle;
+
+        public CircleAngleAnimation(int type, StateCircleAnimation circle, int newAngle) {
+            this.oldAngle = circle.getSweapAngel();
+            this.newAngle = newAngle;
+            this.circle = circle;
+            this.type = type;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation transformation) {
+            float angle;
+            switch (type) {
+                case ZORDER_NORMAL:
+                    angle = oldAngle + ((newAngle - oldAngle) * interpolatedTime);
+                    break;
+                case REVERSE:
+                    angle = oldAngle - ((oldAngle - newAngle) * interpolatedTime);
+                    break;
+                default:
+                    angle = oldAngle + ((newAngle - oldAngle) * interpolatedTime);
+                    break;
+            }
+
+            circle.setSweapAngel(angle);
+            circle.requestLayout();
+        }
+    }
+}
